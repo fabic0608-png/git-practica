4300b1df204027316e0e23ebfc4d3b0e9f5a345c
fabic0608-png <fabic0608@gmail.com>
2026-06-17 18:17:13 -0500

    Add files via upload
    clases herencia

---
+++ ejercicio4/ejercicio4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Persona
+{
+    protected int edad;
+
+    public void Saludar()
+    {
+        Console.WriteLine("Hola");
+    }
+
+    public void SetEdad(int edad)
+    {
+        this.edad = edad;
+    }
+}
+
+class Estudiante : Persona
+{
+    public void Estudiar()
+    {
+        Console.WriteLine("Estoy estudiando");
+    }
+
+    public void VerEdad()
+    {
+        Console.WriteLine("Mi edad es: " + edad + " años");
+    }
+}
+
+class Profesor : Persona
+{
+    public void Explicar()
+    {
+        Console.WriteLine("Estoy explicando");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona();
+        persona.Saludar();
+
+        Estudiante estudiante = new Estudiante();
+        estudiante.SetEdad(16);
+        estudiante.Saludar();
+        estudiante.VerEdad();
+        estudiante.Estudiar();
+
+        Profesor profesor = new Profesor();
+        profesor.SetEdad(40);
+        profesor.Saludar();
+        profesor.Explicar();
+
+        Console.ReadKey();
+    }
+}
+
+
